115edc27fbb1566b64f808597e991abb8faf8044
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-05-14 00:38:07 +0200

    all regions
    all regions

--- world_views/entertainment.php
+++ world_views/entertainment.php
@@ -16,24 +16,43 @@ $radius_color ="rgb(0,0,0)";
 $descript =" ";
 
 //tier 1
+//jousting
 if ($ride =="joust"){
 $stroke_width++;
 $radius_size--;
 $radius_color ="rgb(192,0,0)";
 $descript ="jousting";
 }
+//taverns and inns
 elseif ($ride =="tavern"){
 $stroke_width++;
 $radius_size--;
+$radius_color ="rgb(192,0,0)";
 $radius_color ="rgb(0,192,192)";
 $descript ="tavern";
 }
+elseif ($ride =="coaching"){
+$stroke_width++;
+$radius_size--;
+$radius_color ="rgb(192,0,0)";
+$radius_color ="rgb(0,128,128)";
+$descript ="coaching inn";
+}
+elseif ($ride =="caravan"){
+$stroke_width++;
+$radius_size--;
+$radius_color ="rgb(192,0,0)";
+$radius_color ="rgb(0,64,64)";
+$descript ="caravaners inn";
+}
+//singing
 elseif ($ride =="minstrel"){
 $stroke_width++;
 $radius_size--;
 $radius_color ="rgb(255,0,255)";
 $descript ="minstrel";
 }
+//hunting
 elseif ($ride =="hunt"){
 $stroke_width++;
 $radius_size--;
@@ -46,16 +65,34 @@ $radius_size--;
 $radius_color ="rgb(128,64,32)";
 $descript ="falconry";
 }
+//market
 elseif ($ride =="fair"){
 $stroke_width++;
 $radius_size--;
+$radius_color ="rgb(64,0,64)";
 $radius_color ="rgb(255,255,0)";
-$descript ="fairs";
+$descript ="market fairs";
 }
+elseif ($ride =="bazaar"){
+$stroke_width++;
+$radius_size--;
+$radius_color ="rgb(64,0,64)";
+$radius_color ="rgb(192,192,0)";
+$descript ="bazaar";
+}
+//pelgrimage
 elseif ($ride =="reliquary"){
 $stroke_width++;
 $radius_size--;
+$radius_color ="rgb(192,0,192)";
 $radius_color ="rgb(192,192,192)";
+$descript ="pelgrimage";
+}
+elseif ($ride =="pilgrimage"){
+$stroke_width++;
+$radius_size--;
+$radius_color ="rgb(192,0,192)";
+$radius_color ="rgb(128,128,128)";
 $descript ="pelgrimage";
 }
 else {
--- world_views/view_scents.php
+++ world_views/view_scents.php
@@ -7,16 +7,35 @@ $placename = $town->place;
 $lon = $town->Point->xc;
 $lat = $town->Point->yc;
 
-$agri_a = $town->capital->factory;
-$agri_b = $town->castle->factory;
-$agri_c = $town->town->factory;
-$agri_d = $town->abbey->factory;
+$agri_a = $town->capital->com;
+$agri_b = $town->castle->com;
+$agri_c = $town->town->com;
+$agri_d = $town->abbey->com;
 
-if ($agri_a =="frankincense" || $agri_b =="frankincense" || $agri_c =="frankincense" || $agri_d =="frankincense"){
+$town_a = $town->capital->factory;
+$town_b = $town->castle->factory;
+$town_c = $town->town->factory;
+$town_d = $town->abbey->factory;
+
+if ($agri_a =="olive_oil" || $agri_b =="olive_oil" || $agri_c =="olive_oil" || $agri_d =="olive_oil"){
+$olive_oil ="olive_oil";	
+}
+else {
+$olive_oil ="none";		
+}
+
+if ($town_a =="frankincense" || $town_b =="frankincense" || $town_c =="frankincense" || $town_d =="frankincense"){
 $frankincense ="frankincense";	
 }
 else {
 $frankincense ="none";		
+}
+
+if ($town_a =="soapmaker" || $town_b =="soapmaker" || $town_c =="soapmaker" || $town_d =="soapmaker"){
+$soapmaker ="soapmaker";	
+}
+else {
+$soapmaker ="none";		
 }
 
 $stroke_width =0;
@@ -31,6 +50,44 @@ $stroke_width++;
 $radius_size--;
 $radius_color ="rgb(192,0,0)";
 $descript ="frankincense";
+}
+else {
+$stroke_width =$stroke_width;
+$radius_size =$radius_size;
+$stroke_color = $stroke_color;
+$radius_color =$radius_color;
+$descript =$descript;
+}
+
+//tier 2
+if ($olive_oil =="olive_oil"){
+$stroke_width++;
+$radius_size--;
+$stroke_color = $radius_color;
+$radius_color ="rgb(192,192,0)";
+if ($radius_size<7){
+$descript = $descript." and olive oil";}
+else {
+$descript ="olive oil";}
+}
+else {
+$stroke_width =$stroke_width;
+$radius_size =$radius_size;
+$stroke_color = $stroke_color;
+$radius_color =$radius_color;
+$descript =$descript;
+}
+
+//tier 3
+if ($soapmaker =="soapmaker"){
+$stroke_width++;
+$radius_size--;
+$stroke_color = $radius_color;
+$radius_color ="rgb(0,192,192)";
+if ($radius_size<7){
+$descript = $descript." and soap";}
+else {
+$descript ="soap";}
 }
 else {
 $stroke_width =$stroke_width;
