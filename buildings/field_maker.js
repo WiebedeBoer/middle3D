580944b7e86ab15574377c777de3f11b724a74e3
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-05-19 14:24:04 +0200

    builds
    builds

---
+++ buildings/field_maker.js
@@ -0,0 +1,273 @@
+function fieldMaker(type,xPos,zPos){
+
+//wheat field
+if (type =="grain"){
+    
+        var field1 = new Wheat(xPos+15,0,zPos-15);
+        scene.add( field1 );
+        var field2 = new Wheat(xPos+15,0,zPos);
+        scene.add( field2 );
+        var field3 = new Wheat(xPos+15,0,zPos+15);
+        scene.add( field3 );
+        var field4 = new Wheat(xPos,0,zPos-15);
+        scene.add( field4 );
+        var field5 = new Wheat(xPos,0,zPos); 
+        scene.add( field5 );
+        var field6 = new Wheat(xPos,0,zPos+15);
+        scene.add( field6 );
+        var field7 = new Wheat(xPos-15,0,zPos-15);
+        scene.add( field7 );
+        var field8 = new Wheat(xPos-15,0,zPos);
+        scene.add( field8 );
+        var field9 = new Wheat(xPos-15,0,zPos+15);
+        scene.add( field9 );   
+
+}
+else if (type =="vegetables"){
+    var field1 = new Cabbage(xPos+15,0,zPos-15,0,0,0);
+    scene.add( field1 );
+    var field2 = new Cabbage(xPos+15,0,zPos,0,0,0);
+    scene.add( field2 );
+    var field3 = new Cabbage(xPos+15,0,zPos+15,0,0,0);
+    scene.add( field3 );
+    var field4 = new Cabbage(xPos,0,zPos-15,0,0,0);
+    scene.add( field4 );
+    var field5 = new Cabbage(xPos,0,zPos,0,0,0); 
+    scene.add( field5 );
+    var field6 = new Cabbage(xPos,0,zPos+15,0,0,0);
+    scene.add( field6 );
+    var field7 = new Cabbage(xPos-15,0,zPos-15,0,0,0);
+    scene.add( field7 );
+    var field8 = new Cabbage(xPos-15,0,zPos,0,0,0);
+    scene.add( field8 );
+    var field9 = new Cabbage(xPos-15,0,zPos+15,0,0,0);
+    scene.add( field9 );
+
+}
+//olive grove
+else if (type=="olive"){
+    
+        var Olive1 = new OliveTree(xPos-15,5,zPos+15); 
+        scene.add(Olive1);
+        var Olive2 = new OliveTree(xPos-15,5,zPos-15); 
+        scene.add(Olive2);
+        var Olive3 = new OliveTree(xPos+15,5,zPos+15); 
+        scene.add(Olive3);
+        var Olive4 = new OliveTree(xPos+15,5,zPos-15); 
+        scene.add(Olive4);    
+
+}
+//grapevine
+else if (type=="grape"){   
+        var grape1 = new GrapeVine(xPos+15,0,zPos-15);
+        scene.add( grape1 );
+        var grape2 = new GrapeVine(xPos+15,0,zPos);
+        scene.add( grape2 );
+        var grape3 = new GrapeVine(xPos+15,0,zPos+15);
+        scene.add( grape3 );
+        var grape4 = new GrapeVine(xPos,0,zPos-15);
+        scene.add( grape4 );
+        var grape5 = new GrapeVine(xPos,0,zPos); 
+        scene.add( grape5 );
+        var grape6 = new GrapeVine(xPos,0,zPos+15);
+        scene.add( grape6 );
+        var grape7 = new GrapeVine(xPos-15,0,zPos-15);
+        scene.add( grape7 );
+        var grape8 = new GrapeVine(xPos-15,0,zPos);
+        scene.add( grape8 );
+        var grape9 = new GrapeVine(xPos-15,0,zPos+15);
+        scene.add( grape9 );  
+}
+//beekeeping
+else if(type=="honey"){
+
+    if (climate =="desert" || climate =="desert_marshes"){        
+        var tree1 = new Forest(xPos,45,zPos+15,"tree_palm"); 
+        scene.add(tree1);
+    }
+    else {
+        if (herb =="cedar"){
+            var tree1 = new Forest(xPos,45,zPos+15,"cedar"); 
+            scene.add(tree1);
+        }
+        else if (herb =="pine"){
+            var tree1 = new Forest(xPos,45,zPos+15,"tree_pine"); 
+            scene.add(tree1);
+        }
+        else {
+            var tree1 = new Forest(xPos,45,zPos+15,"tree_transparent"); 
+            scene.add(tree1);
+        }        
+    } 
+        var beehive1 = new Beehive(xPos,zPos-15); 
+        scene.add(beehive1);   
+}
+//forest
+else if(type=="forest"){
+
+    if (climate =="desert" || climate =="desert_marshes"){        
+        var tree1 = new Forest(xPos,45,zPos,"tree_palm"); 
+        scene.add(tree1);
+    }
+    else {
+        if (herb =="cedar"){
+            var tree1 = new Forest(xPos,45,zPos,"cedar"); 
+            scene.add(tree1);
+        }
+        else if (herb =="pine"){
+            var tree1 = new Forest(xPos,45,zPos,"tree_pine"); 
+            scene.add(tree1);
+        }
+        else {
+            var tree1 = new Forest(xPos,45,zPos,"tree_transparent"); 
+            scene.add(tree1);
+        }        
+    } 
+
+}
+//goat pasture
+else if(type =="goat"){
+    var Goat1 = new Goat(xPos-15,5,zPos+15); 
+    scene.add(Goat1);
+    var Goat2 = new Goat(xPos-15,5,zPos-15); 
+    scene.add(Goat2);
+    var Goat3 = new Goat(xPos+15,5,zPos+15); 
+    scene.add(Goat3);
+    var Goat4 = new Goat(xPos+15,5,zPos-15); 
+    scene.add(Goat4);
+    var fenceL2 = new Fence(xPos,0,zPos+30,0,0,0); 
+    scene.add(fenceL2);
+    var fenceR2 = new Fence(xPos,0,zPos-30,0,0,0); 
+    scene.add(fenceR2);
+    var fenceF2 = new Fence(xPos-30,0,zPos,0,1.5707963,0); 
+    scene.add(fenceF2);
+    var fenceB2 = new Fence(xPos+30,0,zPos,0,1.5707963,0); 
+    scene.add(fenceB2);   
+}
+//sheep pasture
+else if(type =="sheep"){
+    
+        var Sheep1 = new Sheep(xPos-15,5,zPos+15); 
+        scene.add(Sheep1);
+        var Sheep2 = new Sheep(xPos-15,5,zPos-15); 
+        scene.add(Sheep2);
+        var Sheep3 = new Sheep(xPos+15,5,zPos+15); 
+        scene.add(Sheep3);
+        var Sheep4 = new Sheep(xPos+15,5,zPos-15); 
+        scene.add(Sheep4);
+        var fenceL2 = new Fence(xPos,0,zPos+30,0,0,0); 
+        scene.add(fenceL2);
+        var fenceR2 = new Fence(xPos,0,zPos-30,0,0,0); 
+        scene.add(fenceR2);
+        var fenceF2 = new Fence(xPos-30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceF2);
+        var fenceB2 = new Fence(xPos+30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceB2);  
+    
+}
+//pig pasture
+else if(type =="pig"){
+    
+        var Pig1 = new Pig(xPos-15,5,zPos+15); 
+        scene.add(Pig1);
+        var Pig2 = new Pig(xPos-15,5,zPos-15); 
+        scene.add(Pig2);
+        var Pig3 = new Pig(xPos+15,5,zPos+15); 
+        scene.add(Pig3);
+        var Pig4 = new Pig(xPos+15,5,zPos-15); 
+        scene.add(Pig4);
+        var fenceL2 = new Fence(xPos,0,zPos+30,0,0,0); 
+        scene.add(fenceL2);
+        var fenceR2 = new Fence(xPos,0,zPos-30,0,0,0); 
+        scene.add(fenceR2);
+        var fenceF2 = new Fence(xPos-30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceF2);
+        var fenceB2 = new Fence(xPos+30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceB2);   
+    
+}
+//chicken pen
+else if(type =="chicken"){
+        var chicken1 = new Chicken(xPos-15,5,zPos+15); 
+        scene.add(chicken1);
+        var chicken2 = new Chicken(xPos-15,5,zPos-15); 
+        scene.add(chicken2);
+        var chicken3 = new Chicken(xPos+15,5,zPos+15); 
+        scene.add(chicken3);
+        var rooster4 = new Rooster(xPos+15,5,zPos-15); 
+        scene.add(rooster4);
+        var chicken5 = new Chicken(xPos,5,zPos-15); 
+        scene.add(chicken5);
+        var chicken6 = new Chicken(xPos,5,zPos+15); 
+        scene.add(chicken6);
+        var fenceL2 = new Fence(xPos,0,zPos+30,0,0,0); 
+        scene.add(fenceL2);
+        var fenceR2 = new Fence(xPos,0,zPos-30,0,0,0); 
+        scene.add(fenceR2);
+        var fenceF2 = new Fence(xPos-30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceF2);
+        var fenceB2 = new Fence(xPos+30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceB2);    
+}
+//duck
+else if(type =="duck"){
+    
+    var duck1 = new Duck(xPos-15,zPos+15); 
+    scene.add(duck1);
+    var duck2 = new Duck(xPos-15,zPos-15); 
+    scene.add(duck2);
+    var duck3 = new Duck(xPos+15,zPos+15); 
+    scene.add(duck3);
+    var duck4 = new Duck(xPos+15,zPos-15); 
+    scene.add(duck4);
+    var duck5 = new Duck(xPos,zPos-15); 
+    scene.add(duck5);
+    var duck6 = new Duck(xPos,zPos+15); 
+    scene.add(duck6);
+    
+    var fenceL2 = new Fence(xPos,0,zPos+30,0,0,0); 
+    scene.add(fenceL2);
+    var fenceR2 = new Fence(xPos,0,zPos-30,0,0,0); 
+    scene.add(fenceR2);
+    var fenceF2 = new Fence(xPos-30,0,zPos,0,1.5707963,0); 
+    scene.add(fenceF2);
+    var fenceB2 = new Fence(xPos+30,0,zPos,0,1.5707963,0); 
+    scene.add(fenceB2); 
+
+}
+//cow pasture
+else if(type =="cow"){
+    
+        var Cow1 = new Cow(xPos-15,5,zPos); 
+        scene.add(Cow1);
+        var Cow2 = new Cow(xPos+15,5,zPos); 
+        scene.add(Cow2);
+        var fenceL2 = new Fence(xPos,0,zPos+30,0,0,0); 
+        scene.add(fenceL2);
+        var fenceR2 = new Fence(xPos,0,zPos-30,0,0,0); 
+        scene.add(fenceR2);
+        var fenceF2 = new Fence(xPos-30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceF2);
+        var fenceB2 = new Fence(xPos+30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceB2);  
+    
+}
+//horse pasture
+else if(type =="horse"){
+
+        var Horse1 = new Horse(xPos-15,5,zPos); 
+        scene.add(Horse1);
+        var Horse2 = new Horse(xPos+15,5,zPos); 
+        scene.add(Horse2);
+        var fenceL2 = new Fence(xPos,0,zPos+30,0,0,0); 
+        scene.add(fenceL2);
+        var fenceR2 = new Fence(xPos,0,zPos-30,0,0,0); 
+        scene.add(fenceR2);
+        var fenceF2 = new Fence(xPos-30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceF2);
+        var fenceB2 = new Fence(xPos+30,0,zPos,0,1.5707963,0); 
+        scene.add(fenceB2);   
+  
+}
+
+}
---
+++ buildings/house_maker.js
@@ -0,0 +1,71 @@
+function houseMaker(type,xPos,zPos){
+var househigh = 21.0;
+
+//COMMERCE PLOT
+//house
+if (type =="villagehouse"){
+var farmhouse = new Villa(xPos+28,househigh,zPos+30,0);
+scene.add( farmhouse );  
+}
+//house
+else if (type =="villagehouse2"){
+var farmhouse = new Villa(xPos+20,househigh,zPos-28,Math.PI/2);
+scene.add( farmhouse );
+}
+//tavern
+else if (type =="tavern"){
+var tavern = new Villa(xPos+28,househigh,zPos+30,0);
+scene.add( tavern );
+}
+//potterer
+else if (type =="potterer"){
+    var potterer = new Villa(xPos+20,househigh,zPos-28,Math.PI/2);
+    scene.add( potterer );  
+}
+//smithy
+else if(type =="smithy"){
+var smith = new Smithy(xPos-30,househigh,zPos+25,-Math.PI/2);
+scene.add( smith ); 
+var quiverbow = new Quiver(xPos+10, 2.5,zPos+33, 0,0,0);
+scene.add( quiverbow ); 
+var arrowbow = new Arrow(xPos+11.5, 7.5,zPos+32.9, -3.14159 / 2,0,0);
+scene.add( arrowbow );
+var arrowLoafCol = new propCollect(xPos+10, 6, zPos+33, "bow"); 
+scene.add(arrowLoafCol);
+}
+//FARM PLOT
+//farm
+else if (type =="farmhouse"){
+var farmhouse = new Villa(xPos+28,househigh,zPos+30,0);
+scene.add( farmhouse );    
+}
+//barn
+else if(type =="barn"){
+    var stables = new Barn(xPos+13,0,zPos);
+    scene.add( stables ); 
+}
+else if(type =="barn2"){
+    var stables = new Barn(xPos+13,0,zPos);
+    scene.add( stables ); 
+}
+//OUTER PLOT
+//forester
+else if (type =="carpenter"){
+    if(climate =="temperate_woodlands" || climate =="temperate_woodlands_marshes" || climate =="mediterranean_woodlands" || climate =="mediterranean_woodlands_marshes"){
+        var carpenter = new Villa(xPos+20,househigh,zPos-28,Math.PI/2);
+        scene.add( carpenter );
+        var stool = new Stool(xPos-5,3,zPos-33,0,0,0);
+        scene.add( stool ); 
+    }
+}
+//basketmaker
+else if (type =="basketmaker"){
+    if(climate =="temperate_woodlands_marshes" || climate =="desert_marshes" || climate =="mediterranean_woodlands_marshes"){
+        var carpenter = new Villa(xPos+20,househigh,zPos-28,Math.PI/2);
+        scene.add( carpenter );
+        var stool = new Stool(xPos-5,3,zPos-33,0,0,0);
+        scene.add( stool ); 
+    }
+}
+
+}
